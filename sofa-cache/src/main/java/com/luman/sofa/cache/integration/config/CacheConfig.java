53084c5c448d7d3643f71fb4d4b0620a642e6336
yeyinghao <1113531030@qq.com>
2024-12-06 22:03:02 +0800

    初始化

--- sofa-cache/src/main/java/com/luman/sofa/cache/integration/config/CacheConfig.java
+++ sofa-cache/src/main/java/com/luman/sofa/cache/integration/config/CacheConfig.java
@@ -1,19 +1,11 @@
 package com.luman.sofa.cache.integration.config;
 
-import cn.hutool.core.map.MapUtil;
 import com.luman.sofa.common.constant.CommConstant;
-import com.luman.sofa.common.utils.LoggerUtil;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import org.redisson.api.RedissonClient;
-import org.redisson.spring.cache.RedissonSpringCacheManager;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
-
-import java.util.Map;
 
 
 /**
@@ -35,44 +27,9 @@ public class CacheConfig {
 	private String projectKeyPrefix;
 
 	/**
-	 * 缓存key前缀
-	 */
-	private String cacheKeyPrefix;
-
-	/**
-	 * TTL毫秒
-	 */
-	private Long ttlMillSecond;
-
-	/**
-	 * 最大空闲时间毫秒
-	 */
-	private Long maxIdleTimeMillSecond;
-
-	/**
 	 * 默认过期秒
 	 */
 	private Long defaultExpiredSecond;
-
-	/**
-	 * 缓存管理器
-	 *
-	 * @param redissonClient redisson客户
-	 * @return {@link CacheManager}
-	 */
-	@Bean
-	public CacheManager cacheManager(RedissonClient redissonClient) {
-		try {
-			Map<String, org.redisson.spring.cache.CacheConfig> config = MapUtil.newHashMap();
-			// 创建一个名称为"testMap"的缓存，过期时间ttl为24分钟，同时最长空闲时maxIdleTime为12分钟。
-			org.redisson.spring.cache.CacheConfig cacheConfig = new org.redisson.spring.cache.CacheConfig(ttlMillSecond, maxIdleTimeMillSecond);
-			config.put(getRealKey(cacheKeyPrefix), cacheConfig);
-			return new RedissonSpringCacheManager(redissonClient, config);
-		} catch (Throwable e) {
-			LoggerUtil.error(log, e);
-		}
-		return null;
-	}
 
 	/**
 	 * 获取真正key
