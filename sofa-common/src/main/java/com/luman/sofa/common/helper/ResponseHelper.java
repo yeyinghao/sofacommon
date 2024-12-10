2d9597028983b9813ae209c903be9f3d26c52af5
yeyinghao <1113531030@qq.com>
2024-12-10 23:22:53 +0800

    初始化

--- sofa-common/src/main/java/com/luman/sofa/common/helper/ResponseHelper.java
+++ sofa-common/src/main/java/com/luman/sofa/common/helper/ResponseHelper.java
@@ -1,9 +1,10 @@
 package com.luman.sofa.common.helper;
 
 import cn.hutool.core.util.StrUtil;
-import com.luman.sofa.dto.enums.ByErrorCode;
 import com.luman.sofa.common.enums.ErrorEnum;
+import com.luman.sofa.common.utils.TracerUtil;
 import com.luman.sofa.dto.Response;
+import com.luman.sofa.dto.enums.ByErrorCode;
 
 public class ResponseHelper {
 
@@ -13,30 +14,40 @@ public class ResponseHelper {
 	 * @return {@link Response }
 	 */
 	public static <T> Response<T> success(T data) {
-		Response<T> response = new Response<>();
-		response.setCode(ErrorEnum.SUCCESS.getHttpCode());
+		Response<T> response = buildResp(ErrorEnum.SUCCESS);
 		response.setSuccess(Boolean.TRUE);
 		response.setData(data);
 		return response;
+	}
+
+	private static <T> Response<T> buildResp(ByErrorCode byErrorCode) {
+		Response<T> resp = new Response<>();
+		resp.setCode(byErrorCode.getHttpCode());
+		resp.setErrCode(byErrorCode.getCode());
+		resp.setErrMessage(byErrorCode.getDesc());
+		resp.setTimestamp(System.currentTimeMillis());
+		resp.setTraceId(TracerUtil.getThreadTraceId());
+		return resp;
 	}
 
 	/**
 	 * 失败
 	 *
 	 * @param byErrorCode 错误枚举
-	 * @param message   子的错误消息
+	 * @param message     子的错误消息
 	 * @return {@link Response }
 	 */
 	public static <T> Response<T> fail(ByErrorCode byErrorCode, String message) {
-		Response<T> response = new Response<>();
-		response.setCode(byErrorCode.getHttpCode());
+		Response<T> response = buildResp(byErrorCode);
 		response.setSuccess(Boolean.FALSE);
 		StringBuilder msg = new StringBuilder();
 		if (StrUtil.isNotBlank(message)) {
 			msg.append(message);
 		}
 		response.setErrCode(byErrorCode.getCode());
-		response.setErrMessage(msg.toString());
+		if (StrUtil.isNotBlank(msg.toString())) {
+			response.setErrMessage(msg.toString());
+		}
 		return response;
 	}
 
@@ -47,11 +58,8 @@ public class ResponseHelper {
 	 * @return {@link Response }
 	 */
 	public static <T> Response<T> fail(ByErrorCode byErrorCode) {
-		Response<T> response = new Response<>();
-		response.setCode(byErrorCode.getHttpCode());
+		Response<T> response = buildResp(byErrorCode );
 		response.setSuccess(Boolean.FALSE);
-		response.setErrCode(byErrorCode.getCode());
-		response.setErrMessage(byErrorCode.getDesc());
 		return response;
 	}
 
